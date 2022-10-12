23a31427095d2508f46555f5d97df6351656cdc1
spyOP <sadinzero07@gmail.com>
2022-10-13 03:34:55 +0500

    commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,8 +11,6 @@ import {
 import { NavLink } from "react-router-dom";
 
 const Sidebar = ({ children }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(!isOpen);
   const menuItem = [
     {
       path: "/",
@@ -47,15 +45,7 @@ const Sidebar = ({ children }) => {
   ];
   return (
     <div className="container">
-      <div style={{ width: isOpen ? "200px" : "50px" }} className="sidebar">
-        <div className="top_section">
-          <h1 style={{ display: isOpen ? "block" : "none" }} className="logo">
-            Logo
-          </h1>
-          <div style={{ marginLeft: isOpen ? "50px" : "0px" }} className="bars">
-            <FaBars onClick={toggle} />
-          </div>
-        </div>
+      <div className="sidebar">
         {menuItem.map((item, index) => (
           <NavLink
             to={item.path}
@@ -64,12 +54,7 @@ const Sidebar = ({ children }) => {
             activeclassName="active"
           >
             <div className="icon">{item.icon}</div>
-            <div
-              style={{ display: isOpen ? "block" : "none" }}
-              className="link_text"
-            >
-              {item.name}
-            </div>
+            <div className="link_text">{item.name}</div>
           </NavLink>
         ))}
       </div>
