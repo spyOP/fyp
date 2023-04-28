6dd69cda0f216053a24f6faa33c2e5849382740c
Sadin Malik <MY_NAME@example.com>
2023-04-28 14:17:40 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import logger from "./services/logService";
-
-logger.init();
 
 ReactDOM.render(<App />, document.getElementById("root"));
-reportWebVitals();
